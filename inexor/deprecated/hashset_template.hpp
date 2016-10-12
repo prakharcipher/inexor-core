965b83246f73e7529891a2d9bc075fbb5e466a40
IAmNotHanni <cmdfreak@web.de>
2016-10-12 22:55:39 +0200

    Small bug fixes and adaptions. No stable build yet.

--- inexor/deprecated/hashset_template.hpp
+++ inexor/deprecated/hashset_template.hpp
@@ -2,11 +2,91 @@
 
 #include "inexor/macros/memfree_macros.hpp"
 #include "inexor/macros/loop_macros.hpp"
+#include "inexor/deprecated/old_string.hpp"
 #include <algorithm>
+#include <SDL_opengl.h>
 
 namespace inexor {
 namespace server {
 
+
+    // simple bernstein hashing algorithm
+    // invented by Dan Bernstein
+    static inline uint hthash(const char *key)
+    {
+        uint h = 5381;
+        for(int i = 0, k; (k = key[i]); i++) h = ((h<<5)+h)^k;    /// bernstein k=33 xor
+        return h;
+    }
+
+    static inline bool htcmp(const char *x, const char *y)
+    {
+        return !strcmp(x, y);
+    }
+
+    struct stringslice
+    {
+        const char *str;
+        int len;
+        stringslice() {}
+        stringslice(const char *str, int len) : str(str), len(len) {}
+        stringslice(const char *str, const char *end) : str(str), len(int(end-str)) {}
+
+        const char *end() const { return &str[len]; }
+    };
+
+    inline char *newstring(const stringslice &s) { return newstring(s.str, s.len); }
+    inline const char *stringptr(const char *s) { return s; }
+    inline const char *stringptr(const stringslice &s) { return s.str; }
+    inline int stringlen(const char *s) { return int(strlen(s)); }
+    inline int stringlen(const stringslice &s) { return s.len; }
+
+    inline char *copystring(char *d, const stringslice &s, size_t len)
+    {
+        size_t slen = std::min(size_t(s.len), len-1);
+        memcpy(d, s.str, slen);
+        d[slen] = 0;
+        return d;
+    }
+    template<size_t N> inline char *copystring(char (&d)[N], const stringslice &s) { return copystring(d, s, N); }
+
+    static inline uint memhash(const void *ptr, int len)
+    {
+        const uchar *data = (const uchar *)ptr;
+        uint h = 5381;
+        loopi(len) h = ((h<<5)+h)^data[i];
+        return h;
+    }
+
+    static inline uint hthash(const stringslice &s) { return memhash(s.str, s.len); }
+
+    static inline bool htcmp(const stringslice &x, const char *y)
+    {
+        return x.len == (int)strlen(y) && !memcmp(x.str, y, x.len);
+    }
+
+    static inline uint hthash(int key)
+    {
+        return key;
+    }
+
+    static inline bool htcmp(int x, int y)
+    {
+        return x==y;
+    }
+
+
+    static inline uint hthash(GLuint key)
+    {
+        return key;
+    }
+
+    static inline bool htcmp(GLuint x, GLuint y)
+    {
+        return x==y;
+    }
+
+    /// 
     template<class H, class E, class K, class T> struct hashbase
     {
         typedef E elemtype;
