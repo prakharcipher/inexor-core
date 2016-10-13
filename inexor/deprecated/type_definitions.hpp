0793f348280bdfe2333184942732bcf77eb519cc
IAmNotHanni <cmdfreak@web.de>
2016-10-13 23:53:44 +0200

    Moved type definition dependency from tools.hpp to deprecated/type_definitions.hpp.

--- inexor/deprecated/type_definitions.hpp
+++ inexor/deprecated/type_definitions.hpp
@@ -1,14 +1,10 @@
 #pragma once
 
-namespace inexor {
-namespace server {
+// no namespace needed
 
-    typedef unsigned char uchar;
-    typedef unsigned short ushort;
-    typedef unsigned int uint;
-    typedef unsigned long ulong;
-    typedef signed long long int llong;
-    typedef unsigned long long int ullong;
-
-};
-};
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+typedef signed long long int llong;
+typedef unsigned long long int ullong;
--- inexor/shared/tools.hpp
+++ inexor/shared/tools.hpp
@@ -13,12 +13,7 @@
 #include "inexor/util/random.hpp"
 #include "inexor/util.hpp"
 
-typedef unsigned char uchar;
-typedef unsigned short ushort;
-typedef unsigned int uint;
-typedef unsigned long ulong;
-typedef signed long long int llong;
-typedef unsigned long long int ullong;
+#include "inexor/deprecated/type_definitions.hpp"
 
 
 #ifdef _DEBUG //TODO remove
