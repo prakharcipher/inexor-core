45e5b7971b1e19fde7fa833d3761c213f75fb52c
IAmNotHanni <cmdfreak@web.de>
2016-10-14 00:23:31 +0200

    Fixed deprecated hashset template.

--- inexor/deprecated/hashset_template.hpp
+++ inexor/deprecated/hashset_template.hpp
@@ -8,333 +8,305 @@
 #include <algorithm>
 #include <SDL_opengl.h>
 
-namespace inexor {
-namespace server {
 
-    // simple bernstein hashing algorithm
-    // invented by Dan Bernstein
-    static inline uint hthash(const char *key)
+// simple bernstein hashing algorithm
+// invented by Dan Bernstein
+static inline uint hthash(const char *key)
+{
+    uint h = 5381;
+    for(int i = 0, k; (k = key[i]); i++) h = ((h<<5)+h)^k;    /// bernstein k=33 xor
+    return h;
+}
+
+static inline bool htcmp(const char *x, const char *y)
+{
+    return !strcmp(x, y);
+}
+
+static inline uint memhash(const void *ptr, int len)
+{
+    const uchar *data = (const uchar *)ptr;
+    uint h = 5381;
+    loopi(len) h = ((h<<5)+h)^data[i];
+    return h;
+}
+
+static inline uint hthash(const stringslice &s) { return memhash(s.str, s.len); }
+
+static inline bool htcmp(const stringslice &x, const char *y)
+{
+    return x.len == (int)strlen(y) && !memcmp(x.str, y, x.len);
+}
+
+static inline uint hthash(int key)
+{
+    return key;
+}
+
+/// compares values of x and y
+static inline bool htcmp(int x, int y)
+{
+    return x==y;
+}
+
+
+/// ?
+#ifndef STANDALONE
+static inline uint hthash(GLuint key)
+{
+    return key;
+}
+
+static inline bool htcmp(GLuint x, GLuint y)
+{
+    return x==y;
+}
+#endif
+
+template<class H, class E, class K, class T> struct hashbase
+{
+    typedef E elemtype;
+    typedef K keytype;
+    typedef T datatype;
+
+    enum { CHUNKSIZE = 64 };
+
+    struct chain 
     {
-        uint h = 5381;
-        for(int i = 0, k; (k = key[i]); i++) h = ((h<<5)+h)^k;    /// bernstein k=33 xor
-        return h;
-    }
-
-    static inline bool htcmp(const char *x, const char *y)
+        E elem; 
+        chain *next;
+    };
+    struct chainchunk 
     {
-        return !strcmp(x, y);
-    }
-
-    struct stringslice
-    {
-        const char *str;
-        int len;
-        stringslice() {}
-        stringslice(const char *str, int len) : str(str), len(len) {}
-        stringslice(const char *str, const char *end) : str(str), len(int(end-str)) {}
-
-        const char *end() const { return &str[len]; }
+        chain chains[CHUNKSIZE];
+        chainchunk *next;
     };
 
-    inline char *newstring(const stringslice &s) { return newstring(s.str, s.len); }
-    inline const char *stringptr(const char *s) { return s; }
-    inline const char *stringptr(const stringslice &s) { return s.str; }
-    inline int stringlen(const char *s) { return int(strlen(s)); }
-    inline int stringlen(const stringslice &s) { return s.len; }
+    int size;
+    int numelems;
+    chain **chains;
 
-    inline char *copystring(char *d, const stringslice &s, size_t len)
-    {
-        size_t slen = std::min(size_t(s.len), len-1);
-        memcpy(d, s.str, slen);
-        d[slen] = 0;
-        return d;
-    }
-    template<size_t N> inline char *copystring(char (&d)[N], const stringslice &s) { return copystring(d, s, N); }
+    chainchunk *chunks;
+    chain *unused;
 
-    static inline uint memhash(const void *ptr, int len)
+    enum { DEFAULTSIZE = 1<<10 };
+
+    hashbase(int size = DEFAULTSIZE)
+      : size(size)
     {
-        const uchar *data = (const uchar *)ptr;
-        uint h = 5381;
-        loopi(len) h = ((h<<5)+h)^data[i];
-        return h;
+        numelems = 0;
+        chunks = NULL;
+        unused = NULL;
+        chains = new chain *[size];
+        memset(chains, 0, size*sizeof(chain *));
     }
 
-    static inline uint hthash(const stringslice &s) { return memhash(s.str, s.len); }
-
-    static inline bool htcmp(const stringslice &x, const char *y)
+    ~hashbase()
     {
-        return x.len == (int)strlen(y) && !memcmp(x.str, y, x.len);
+        DELETEA(chains);
+        deletechunks();
     }
 
-    static inline uint hthash(int key)
+    chain *insert(uint h)
     {
-        return key;
-    }
-
-    static inline bool htcmp(int x, int y)
-    {
-        return x==y;
-    }
-
-
-    static inline uint hthash(GLuint key)
-    {
-        return key;
-    }
-
-    static inline bool htcmp(GLuint x, GLuint y)
-    {
-        return x==y;
-    }
-
-    /// 
-    template<class H, class E, class K, class T> struct hashbase
-    {
-        typedef E elemtype;
-        typedef K keytype;
-        typedef T datatype;
-
-        enum { CHUNKSIZE = 64 };
-
-        struct chain 
+        if(!unused)
         {
-            E elem; 
-            chain *next;
-        };
-        struct chainchunk 
-        {
-            chain chains[CHUNKSIZE];
-            chainchunk *next;
-        };
-
-        int size;
-        int numelems;
-        chain **chains;
-
-        chainchunk *chunks;
-        chain *unused;
-
-        enum { DEFAULTSIZE = 1<<10 };
-
-        hashbase(int size = DEFAULTSIZE)
-        : size(size)
-        {
-            numelems = 0;
-            chunks = NULL;
-            unused = NULL;
-            chains = new chain *[size];
-            memset(chains, 0, size*sizeof(chain *));
+            chainchunk *chunk = new chainchunk;
+            chunk->next = chunks;
+            chunks = chunk;
+            loopi(CHUNKSIZE-1) chunk->chains[i].next = &chunk->chains[i+1];
+            chunk->chains[CHUNKSIZE-1].next = unused;
+            unused = chunk->chains;
         }
+        chain *c = unused;
+        unused = unused->next;
+        c->next = chains[h];
+        chains[h] = c;
+        numelems++;
+        return c;
+    }
 
-        ~hashbase()
-        {
-            DELETEA(chains);
-            deletechunks();
-        }
+    template<class U>
+    T &insert(uint h, const U &key)
+    {
+        chain *c = insert(h);
+        H::setkey(c->elem, key);
+        return H::getdata(c->elem);
+    }
 
-        chain *insert(uint h)
+    #define HTFIND(success, fail) \
+        uint h = hthash(key)&(this->size-1); \
+        for(chain *c = this->chains[h]; c; c = c->next) \
+        { \
+            if(htcmp(key, H::getkey(c->elem))) return success H::getdata(c->elem); \
+        } \
+        return (fail);
+
+    template<class U>
+    T *access(const U &key)
+    {
+        HTFIND(&, NULL);
+    }
+
+    template<class U, class V>
+    T &access(const U &key, const V &elem)
+    {
+        HTFIND( , insert(h, key) = elem);
+    }
+
+    template<class U>
+    T &operator[](const U &key)
+    {
+        HTFIND( , insert(h, key));
+    }
+
+    template<class U>
+    T &find(const U &key, T &notfound)
+    {
+        HTFIND( , notfound);
+    }
+
+    template<class U>
+    const T &find(const U &key, const T &notfound)
+    {
+        HTFIND( , notfound);
+    }
+
+    template<class U>
+    bool remove(const U &key)
+    {
+        uint h = hthash(key)&(size-1);
+        for(chain **p = &chains[h], *c = chains[h]; c; p = &c->next, c = c->next)
         {
-            if(!unused)
+            if(htcmp(key, H::getkey(c->elem)))
             {
-                chainchunk *chunk = new chainchunk;
-                chunk->next = chunks;
-                chunks = chunk;
-                loopi(CHUNKSIZE-1) chunk->chains[i].next = &chunk->chains[i+1];
-                chunk->chains[CHUNKSIZE-1].next = unused;
-                unused = chunk->chains;
-            }
-            chain *c = unused;
-            unused = unused->next;
-            c->next = chains[h];
-            chains[h] = c;
-            numelems++;
-            return c;
-        }
-
-        template<class U>
-        T &insert(uint h, const U &key)
-        {
-            chain *c = insert(h);
-            H::setkey(c->elem, key);
-            return H::getdata(c->elem);
-        }
-
-        #define HTFIND(success, fail) \
-            uint h = hthash(key)&(this->size-1); \
-            for(chain *c = this->chains[h]; c; c = c->next) \
-            { \
-                if(htcmp(key, H::getkey(c->elem))) return success H::getdata(c->elem); \
-            } \
-            return (fail);
-
-        template<class U>
-        T *access(const U &key)
-        {
-            HTFIND(&, NULL);
-        }
-
-        template<class U, class V>
-        T &access(const U &key, const V &elem)
-        {
-            HTFIND( , insert(h, key) = elem);
-        }
-
-        template<class U>
-        T &operator[](const U &key)
-        {
-            HTFIND( , insert(h, key));
-        }
-
-        template<class U>
-        T &find(const U &key, T &notfound)
-        {
-            HTFIND( , notfound);
-        }
-
-        template<class U>
-        const T &find(const U &key, const T &notfound)
-        {
-            HTFIND( , notfound);
-        }
-
-        template<class U>
-        bool remove(const U &key)
-        {
-            uint h = hthash(key)&(size-1);
-            for(chain **p = &chains[h], *c = chains[h]; c; p = &c->next, c = c->next)
-            {
-                if(htcmp(key, H::getkey(c->elem)))
-                {
-                    *p = c->next;
-                    c->elem.~E();
-                    new (&c->elem) E;
-                    c->next = unused;
-                    unused = c;
-                    numelems--;
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        void deletechunks()
-        {
-            for(chainchunk *nextchunk; chunks; chunks = nextchunk)
-            {
-                nextchunk = chunks->next;
-                delete chunks;
+                *p = c->next;
+                c->elem.~E();
+                new (&c->elem) E;
+                c->next = unused;
+                unused = c;
+                numelems--;
+                return true;
             }
         }
+        return false;
+    }
 
-        void clear()
-        {
-            if(!numelems) return;
-            memset(chains, 0, size*sizeof(chain *));
-            numelems = 0;
-            unused = NULL;
-            deletechunks();
-        }
-
-        static inline chain *enumnext(void *i) { return ((chain *)i)->next; }
-        static inline K &enumkey(void *i) { return H::getkey(((chain *)i)->elem); }
-        static inline T &enumdata(void *i) { return H::getdata(((chain *)i)->elem); }
-    };
-
-    template<class T> struct hashset : hashbase<hashset<T>, T, T, T>
+    void deletechunks()
     {
-        typedef hashbase<hashset<T>, T, T, T> basetype;
-
-        hashset(int size = basetype::DEFAULTSIZE) : basetype(size) {}
-
-        static inline const T &getkey(const T &elem) { return elem; }
-        static inline T &getdata(T &elem) { return elem; }
-        template<class K> static inline void setkey(T &elem, const K &key) {}
-
-        template<class V>
-        T &add(const V &elem)
+        for(chainchunk *nextchunk; chunks; chunks = nextchunk)
         {
-            return basetype::access(elem, elem);
+            nextchunk = chunks->next;
+            delete chunks;
         }
-    };
+    }
 
-    template<class T> struct hashnameset : hashbase<hashnameset<T>, T, const char *, T>
+    void clear()
     {
-        typedef hashbase<hashnameset<T>, T, const char *, T> basetype;
+        if(!numelems) return;
+        memset(chains, 0, size*sizeof(chain *));
+        numelems = 0;
+        unused = NULL;
+        deletechunks();
+    }
 
-        hashnameset(int size = basetype::DEFAULTSIZE) : basetype(size) {}
-
-        template<class U> static inline const char *getkey(const U &elem) { return elem.name; }
-        template<class U> static inline const char *getkey(U *elem) { return elem->name; }
-        static inline T &getdata(T &elem) { return elem; }
-        template<class K> static inline void setkey(T &elem, const K &key) {}
-
-        template<class V>
-        T &add(const V &elem)
-        {
-            return basetype::access(getkey(elem), elem);
-        }
-    };
-
-    template<class K, class T> struct hashtableentry
-    {
-        K key;
-        T data;
-    };
-
-    template<class K, class T> struct hashtable : hashbase<hashtable<K, T>, hashtableentry<K, T>, K, T>
-    {
-        typedef hashbase<hashtable<K, T>, hashtableentry<K, T>, K, T> basetype;
-        typedef typename basetype::elemtype elemtype;
-
-        hashtable(int size = basetype::DEFAULTSIZE) : basetype(size) {}
-
-        static inline K &getkey(elemtype &elem) { return elem.key; }
-        static inline T &getdata(elemtype &elem) { return elem.data; }
-        template<class U> static inline void setkey(elemtype &elem, const U &key) { elem.key = key; }
-    };
-
-    #define enumeratekt(ht,k,e,t,f,b) loopi((ht).size) for(void *ec = (ht).chains[i]; ec;) { k &e = (ht).enumkey(ec); t &f = (ht).enumdata(ec); ec = (ht).enumnext(ec); b; }
-    #define enumerate(ht,t,e,b)       loopi((ht).size) for(void *ec = (ht).chains[i]; ec;) { t &e = (ht).enumdata(ec); ec = (ht).enumnext(ec); b; }
-
-    struct unionfind
-    {
-        struct ufval
-        {
-            int rank, next;
-
-            ufval() : rank(0), next(-1) {}
-        };
-
-        vector<ufval> ufvals;
-
-        int find(int k)
-        {
-            if(k>=ufvals.length()) return k;
-            while(ufvals[k].next>=0) k = ufvals[k].next;
-            return k;
-        }
-
-        int compressfind(int k)
-        {
-            if(ufvals[k].next<0) return k;
-            return ufvals[k].next = compressfind(ufvals[k].next);
-        }
-
-        void unite (int x, int y)
-        {
-            while(ufvals.length() <= std::max(x, y)) ufvals.add();
-            x = compressfind(x);
-            y = compressfind(y);
-            if(x==y) return;
-            ufval &xval = ufvals[x], &yval = ufvals[y];
-            if(xval.rank < yval.rank) xval.next = y;
-            else
-            {
-                yval.next = x;
-                if(xval.rank==yval.rank) yval.rank++;
-            }
-        }
-    };
-
+    static inline chain *enumnext(void *i) { return ((chain *)i)->next; }
+    static inline K &enumkey(void *i) { return H::getkey(((chain *)i)->elem); }
+    static inline T &enumdata(void *i) { return H::getdata(((chain *)i)->elem); }
 };
+
+template<class T> struct hashset : hashbase<hashset<T>, T, T, T>
+{
+    typedef hashbase<hashset<T>, T, T, T> basetype;
+
+    hashset(int size = basetype::DEFAULTSIZE) : basetype(size) {}
+
+    static inline const T &getkey(const T &elem) { return elem; }
+    static inline T &getdata(T &elem) { return elem; }
+    template<class K> static inline void setkey(T &elem, const K &key) {}
+
+    template<class V>
+    T &add(const V &elem)
+    {
+        return basetype::access(elem, elem);
+    }
+};
+
+template<class T> struct hashnameset : hashbase<hashnameset<T>, T, const char *, T>
+{
+    typedef hashbase<hashnameset<T>, T, const char *, T> basetype;
+
+    hashnameset(int size = basetype::DEFAULTSIZE) : basetype(size) {}
+
+    template<class U> static inline const char *getkey(const U &elem) { return elem.name; }
+    template<class U> static inline const char *getkey(U *elem) { return elem->name; }
+    static inline T &getdata(T &elem) { return elem; }
+    template<class K> static inline void setkey(T &elem, const K &key) {}
+
+    template<class V>
+    T &add(const V &elem)
+    {
+        return basetype::access(getkey(elem), elem);
+    }
+};
+
+template<class K, class T> struct hashtableentry
+{
+    K key;
+    T data;
+};
+
+template<class K, class T> struct hashtable : hashbase<hashtable<K, T>, hashtableentry<K, T>, K, T>
+{
+    typedef hashbase<hashtable<K, T>, hashtableentry<K, T>, K, T> basetype;
+    typedef typename basetype::elemtype elemtype;
+
+    hashtable(int size = basetype::DEFAULTSIZE) : basetype(size) {}
+
+    static inline K &getkey(elemtype &elem) { return elem.key; }
+    static inline T &getdata(elemtype &elem) { return elem.data; }
+    template<class U> static inline void setkey(elemtype &elem, const U &key) { elem.key = key; }
+};
+
+#define enumeratekt(ht,k,e,t,f,b) loopi((ht).size) for(void *ec = (ht).chains[i]; ec;) { k &e = (ht).enumkey(ec); t &f = (ht).enumdata(ec); ec = (ht).enumnext(ec); b; }
+#define enumerate(ht,t,e,b)       loopi((ht).size) for(void *ec = (ht).chains[i]; ec;) { t &e = (ht).enumdata(ec); ec = (ht).enumnext(ec); b; }
+
+struct unionfind
+{
+    struct ufval
+    {
+        int rank, next;
+
+        ufval() : rank(0), next(-1) {}
+    };
+
+    vector<ufval> ufvals;
+
+    int find(int k)
+    {
+        if(k>=ufvals.length()) return k;
+        while(ufvals[k].next>=0) k = ufvals[k].next;
+        return k;
+    }
+
+    int compressfind(int k)
+    {
+        if(ufvals[k].next<0) return k;
+        return ufvals[k].next = compressfind(ufvals[k].next);
+    }
+
+    void unite (int x, int y)
+    {
+        while(ufvals.length() <= max(x, y)) ufvals.add();
+        x = compressfind(x);
+        y = compressfind(y);
+        if(x==y) return;
+        ufval &xval = ufvals[x], &yval = ufvals[y];
+        if(xval.rank < yval.rank) xval.next = y;
+        else
+        {
+            yval.next = x;
+            if(xval.rank==yval.rank) yval.rank++;
+        }
+    }
 };
