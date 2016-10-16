304d99932a72aaaa4b003e0c68eb66fd5b55b729
IAmNotHanni <cmdfreak@web.de>
2016-10-17 00:32:20 +0200

    Moved hud icon enumeration from game.hpp to enumerations/hud_icons.hpp.

--- inexor/fpsgame/game.hpp
+++ inexor/fpsgame/game.hpp
@@ -107,45 +107,7 @@ static const char * const teamblipcolor[TEAM_NUM] =
     "_red"
 };
 
-
-/// enumeration for icons
-enum
-{
-    HICON_BLUE_ARMOUR = 0,
-    HICON_GREEN_ARMOUR,
-    HICON_YELLOW_ARMOUR,
-
-    HICON_HEALTH,
-
-    HICON_FIST,
-    HICON_SG,
-    HICON_CG,
-    HICON_RL,
-    HICON_RIFLE,
-    HICON_GL,
-    HICON_PISTOL,
-    HICON_BOMB,
-
-    HICON_QUAD,
-
-    HICON_RED_FLAG,
-    HICON_BLUE_FLAG,
-    HICON_NEUTRAL_FLAG,
-
-    HICON_TOKEN,
-	
-	// bomberman
-    HICON_BOMBRADIUS,
-    HICON_BOMBDELAY,
-
-    HICON_X       = 20,
-    HICON_Y       = 1650,
-    HICON_TEXTY   = 1644,
-    HICON_STEP    = 490,
-    HICON_SIZE    = 120,
-    HICON_SPACE   = 40
-};
-
+#include "inexor/enumerations/hud_icons.hpp"
 
 /// Bomberman: HUD announce effects
 enum hudannounceeffects 
