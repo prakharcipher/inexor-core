bdb9c2c6096c7e8112112def1d6ae0c4e8d5435f
IAmNotHanni <cmdfreak@web.de>
2016-10-17 01:07:53 +0200

    Moved old typedefs from tools.hpp to deprecated/type_definitions.hpp.

---
+++ inexor/deprecated/type_definitions.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+/// those typedefs are deprecated because they are duplicates
+/// of definitions in other common header files.
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+typedef signed long long int llong;
+typedef unsigned long long int ullong;
