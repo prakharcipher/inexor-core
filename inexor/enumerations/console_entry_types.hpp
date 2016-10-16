46110c2260b5a04b52c2c2608c23b9697f62320c
IAmNotHanni <cmdfreak@web.de>
2016-10-17 00:05:42 +0200

    Removed namespace inexor::server from entity_types.hpp and console_entry_types.hpp.

--- inexor/enumerations/console_entry_types.hpp
+++ inexor/enumerations/console_entry_types.hpp
@@ -1,18 +1,12 @@
 #pragma once
 
-namespace inexor {
-namespace server {
-
-    /// game console entry types
-    enum
-    {
-        CON_CHAT       = 1<<8,
-        CON_TEAMCHAT   = 1<<9,
-        CON_GAMEINFO   = 1<<10,
-        CON_FRAG_SELF  = 1<<11,
-        CON_FRAG_OTHER = 1<<12,
-        CON_TEAMKILL   = 1<<13
-    };
-
-};
+/// game console entry types
+enum
+{
+    CON_CHAT       = 1<<8,
+    CON_TEAMCHAT   = 1<<9,
+    CON_GAMEINFO   = 1<<10,
+    CON_FRAG_SELF  = 1<<11,
+    CON_FRAG_OTHER = 1<<12,
+    CON_TEAMKILL   = 1<<13
 };
--- inexor/enumerations/entity_types.hpp
+++ inexor/enumerations/entity_types.hpp
@@ -1,51 +1,44 @@
 #pragma once
 
-namespace inexor {
-namespace server {
-        
-    /// entity types (old Cube2 entity system)
-    enum
-    {
-        NOTUSED = ET_EMPTY,
-        LIGHT = ET_LIGHT,
-        MAPMODEL = ET_MAPMODEL,
-        PLAYERSTART,
-        ENVMAP = ET_ENVMAP,
-        PARTICLES = ET_PARTICLES,
-        MAPSOUND = ET_SOUND,
-        SPOTLIGHT = ET_SPOTLIGHT,
-        
-        I_SHELLS,
-        I_BULLETS,
-        I_ROCKETS,
-        I_ROUNDS,
-        I_GRENADES,
-        I_CARTRIDGES,
-        I_BOMBS = ET_BOMBS,
-        I_BOMBRADIUS,
-        I_BOMBDELAY,
-        I_HEALTH,
-        I_BOOST,
-        I_GREENARMOUR,
-        I_YELLOWARMOUR,
-        I_QUAD,
+/// entity types (old Cube2 entity system)
+enum
+{
+    NOTUSED = ET_EMPTY,
+    LIGHT = ET_LIGHT,
+    MAPMODEL = ET_MAPMODEL,
+    PLAYERSTART,
+    ENVMAP = ET_ENVMAP,
+    PARTICLES = ET_PARTICLES,
+    MAPSOUND = ET_SOUND,
+    SPOTLIGHT = ET_SPOTLIGHT,
+    
+    I_SHELLS,
+    I_BULLETS,
+    I_ROCKETS,
+    I_ROUNDS,
+    I_GRENADES,
+    I_CARTRIDGES,
+    I_BOMBS = ET_BOMBS,
+    I_BOMBRADIUS,
+    I_BOMBDELAY,
+    I_HEALTH,
+    I_BOOST,
+    I_GREENARMOUR,
+    I_YELLOWARMOUR,
+    I_QUAD,
 
-        TELEPORT,
-        TELEDEST,
-        MONSTER,
-        CARROT,
-        JUMPPAD,
-        BASE,
-        RESPAWNPOINT,
-        BOX,
-        BARREL,
-        PLATFORM,
-        ELEVATOR,
-        FLAG,
-        OBSTACLE = ET_OBSTACLE,
-        MAXENTTYPES
-    };
-
-
-};
+    TELEPORT,
+    TELEDEST,
+    MONSTER,
+    CARROT,
+    JUMPPAD,
+    BASE,
+    RESPAWNPOINT,
+    BOX,
+    BARREL,
+    PLATFORM,
+    ELEVATOR,
+    FLAG,
+    OBSTACLE = ET_OBSTACLE,
+    MAXENTTYPES
 };
