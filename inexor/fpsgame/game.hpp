6aef9c4773cb9789732fc71f1a7713bf254abd97
IAmNotHanni <cmdfreak@web.de>
2016-10-17 00:33:59 +0200

    Moved bomberman HUD announcement effect enumeration from game.hpp to enumerations/bomberman_hud_announces.hpp.

--- inexor/fpsgame/game.hpp
+++ inexor/fpsgame/game.hpp
@@ -109,23 +109,7 @@ static const char * const teamblipcolor[TEAM_NUM] =
 
 #include "inexor/enumerations/hud_icons.hpp"
 
-/// Bomberman: HUD announce effects
-enum hudannounceeffects 
-{
-    E_STATIC_CENTER = 0,
-    E_STATIC_LEFT,
-    E_STATIC_RIGHT,
-    E_STATIC_TOP,
-    E_STATIC_BOTTOM,
-    E_FADE_LEFT_RIGHT,
-    E_FADE_RIGHT_LEFT,
-    E_FADE_TOP_BOTTOM,
-    E_FADE_BOTTOM_TOP,
-    E_ZOOM_IN,
-    E_ZOOM_OUT,
-    E_BLINK_CENTER
-};
-
+#include "inexor/enumerations/bomberman_hud_announces.hpp"
 
 
 /// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
