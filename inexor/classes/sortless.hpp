b6fbff3e4d9b6cf489d38d8fc5ce1a0320c2f7e8
IAmNotHanni <cmdfreak@web.de>
2016-10-17 01:55:39 +0200

    Moved sortless from tools.hpp to templates/sortless.hpp.

---
+++ inexor/classes/sortless.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+struct sortless
+{
+    template<class T> bool operator()(const T &x, const T &y) const { return x < y; }
+    bool operator()(char *x, char *y) const { return strcmp(x, y) < 0; }
+    bool operator()(const char *x, const char *y) const { return strcmp(x, y) < 0; }
+};
--- inexor/shared/tools.hpp
+++ inexor/shared/tools.hpp
@@ -78,12 +78,7 @@ INEXOR_FUNCTION_ALIAS(detrnd, inexor::util::deterministic_rnd<int>);
 
 #include "inexor/templates/heapscore.hpp"
 
-struct sortless
-{
-    template<class T> bool operator()(const T &x, const T &y) const { return x < y; }
-    bool operator()(char *x, char *y) const { return strcmp(x, y) < 0; }
-    bool operator()(const char *x, const char *y) const { return strcmp(x, y) < 0; }
-};
+#include "inexor/classes/sortless.hpp"
 
 struct sortnameless
 {
