eae4effb6a8a7af34391caa0d25a094b7300420b
IAmNotHanni <cmdfreak@web.de>
2016-10-16 23:49:04 +0200

    Moved game console entry types from game.hpp to console_entry_types.hpp.

--- inexor/fpsgame/game.hpp
+++ inexor/fpsgame/game.hpp
@@ -12,17 +12,7 @@
 #include "inexor/shared/cube.hpp"
 #include "inexor/util/Logging.hpp"
 
-/// game console entry types
-enum
-{
-    CON_CHAT       = 1<<8,
-    CON_TEAMCHAT   = 1<<9,
-    CON_GAMEINFO   = 1<<10,
-    CON_FRAG_SELF  = 1<<11,
-    CON_FRAG_OTHER = 1<<12,
-    CON_TEAMKILL   = 1<<13
-};
-
+#include "inexor/enumerations/console_entry_types.hpp"
 
 /// network quantization scale
 #define DMF 16.0f   /// for world locations
