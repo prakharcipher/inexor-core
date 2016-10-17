fbc177c26a6f9aa8fcc05dabf7a2f6fa73354218
IAmNotHanni <cmdfreak@web.de>
2016-10-17 18:01:26 +0200

    Moved userkey from fpsgame/server.cpp to classes/userkey.hpp.

--- inexor/fpsgame/server.cpp
+++ inexor/fpsgame/server.cpp
@@ -1239,17 +1239,7 @@ namespace server
     
     SVAR(serverauth, "");
 
-    struct userkey
-    {
-        char *name;
-        char *desc;
-        
-        userkey() : name(NULL), desc(NULL) {}
-        userkey(char *name, char *desc) : name(name), desc(desc) {}
-    };
-
-    static inline uint hthash(const userkey &k) { return ::hthash(k.name); }
-    static inline bool htcmp(const userkey &x, const userkey &y) { return !strcmp(x.name, y.name) && !strcmp(x.desc, y.desc); }
+    #include "inexor/classes/userkey.hpp"
 
     struct userinfo : userkey
     {
