67622478c57aa1a1febdd4bbf253fcb95487505c
a_teammate <madoe3@web.de>
2017-05-28 18:05:24 +0200

    [PM] update conan boost package to support vs17

--- dependencies.py
+++ dependencies.py
@@ -2,7 +2,7 @@ requires = (("InexorGlueGen/0.6.0alpha@inexorgame/testing"),
             ("Protobuf/3.1.0@inexorgame/stable"),
             ("gRPC/1.1.0-dev@inexorgame/stable"),
             ("doxygen/1.8.13@inexorgame/testing"),
-            ("Boost/1.60.0@lasote/stable"),
+            ("Boost/1.64.0@inexorgame/stable"),
             ("RapidJSON/1.0.2@inexorgame/stable"),
             ("zlib/1.2.8@lasote/stable"),
             ("gtest/1.7.0@lasote/stable"),
@@ -10,7 +10,7 @@ requires = (("InexorGlueGen/0.6.0alpha@inexorgame/testing"),
             ("spdlog/0.10.0@memsharded/stable"),
             ("SDL2/2.0.5@lasote/stable"),
             ("SDL2_image/2.0.1@lasote/stable"),
-            ("CEF/3.2704.1424.gc3f0a5b@a_teammate/testing")
+            ("CEF/3.2704.1424.gc3f0a5b@inexorgame/stable")
         )
 
 options = '''
