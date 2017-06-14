b7f9a8dcb817dbfe332b940f462c70180ea6225a
a_teammate <madoe3@web.de>
2017-06-14 14:39:20 +0200

    revert: [TRAVIS] always use create-package scope! (reverted from commit 371629be125f9891e96ab14b907b1123681a8268)

--- tool/travis.sh
+++ tool/travis.sh
@@ -223,8 +223,8 @@ build() {
     mkcd "/tmp/inexor-build"
     conan
     echo "executed conan install "$gitroot" --scope build_all=1 --scope create_package=1 --build=missing -s compiler=$CONAN_COMPILER -s compiler.version=$CONAN_COMPILER_VERSION -s compiler.libcxx=libstdc++11"
-    conan install "$gitroot" --scope build_all=1 --scope create_package=1 --build=missing -s compiler="$CONAN_COMPILER" -s compiler.version="$CONAN_COMPILER_VERSION" -s compiler.libcxx="libstdc++11"
-    conan build "$gitroot" --scope create_package=1
+    conan install "$gitroot" --scope build_all=1 --scope create_package=1  --build=missing -s compiler="$CONAN_COMPILER" -s compiler.version="$CONAN_COMPILER_VERSION" -s compiler.libcxx="libstdc++11"
+    conan build "$gitroot"
   )
 }
 
